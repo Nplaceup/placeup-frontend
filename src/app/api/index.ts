af40c1bc8b89ea9ad1b73078bf660b852524eba2
hyejun <hyejuny640@gmail.com>
2026-05-28 17:22:54 +0900

    esc

--- src/app/api/index.ts
+++ src/app/api/index.ts
@@ -7,7 +7,6 @@ function createApi(headers: Record<string, string>) {
   const instance = axios.create({
     baseURL: API_SERVER_URL,
     headers,
-    withCredentials: true,
   });
 
   return instance;
