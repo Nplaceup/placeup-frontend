d0d9b3ff6a0356b7573cf9d61b1237fdb7edee57
hyejun <hyejuny640@gmail.com>
2026-06-08 23:29:46 +0900

    dd

--- src/app/api/index.ts
+++ src/app/api/index.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 //const API_SERVER_URL = 'http://localhost:8080';
 const API_SERVER_URL = 'http://112.186.86.251:8080';
 
+
 function createApi(headers: Record<string, string>) {
   const instance = axios.create({
     baseURL: API_SERVER_URL,
