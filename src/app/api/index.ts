357426a6113ead7d5bcc9947d08bcdb1ea5a8cf9
hyejun <hyejuny640@gmail.com>
2026-06-09 12:26:06 +0900

    서버 주소 변경

--- src/app/api/index.ts
+++ src/app/api/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_SERVER_URL = 'https://annual-sic-christmas-asin.trycloudflare.com'; 
+const API_SERVER_URL = 'https://disk-flow-snow-elements.trycloudflare.com'; 
 
 function createApi(headers: Record<string, string>) {
   const instance = axios.create({
